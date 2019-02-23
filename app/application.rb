c201dd2c24b40763621f3869c0343447c24aea33
Sara Walker <sarawall1187@gmail.com>
2019-02-23 17:04:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.now 
     binding.pry
-    if time < time(12.0/24)
+    if time.hour < 12
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"

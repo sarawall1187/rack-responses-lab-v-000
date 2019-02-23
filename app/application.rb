d728312e7044943b6aad414a57bb8e86620ed529
Sara Walker <sarawall1187@gmail.com>
2019-02-23 16:55:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,11 @@
 class Application 
   def call(env)
     resp = Rack::Response.new
-    
+    time = Time.now 
+    if time < 12
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end
     
 end
